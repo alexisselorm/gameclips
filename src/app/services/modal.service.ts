eacc1406424223866bbac010be138ca10e86867d
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-04-13 19:14:10 +0200

    Modal components ElementRef Clearing memory leaks

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private modals: IModal[] = [];
+  constructor() {}
+
+  isModalOpen(id: string): boolean {
+    // The double exclamation mark forces typescript to return a boolean if the modal isn't found. Double exclamation mark -> make it false->bring it back to true
+    return !!this.modals.find((modal) => modal.id === id)?.visible;
+  }
+  toggleModal(id: string) {
+    const modal = this.modals.find((modal) => modal.id);
+
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
+  }
+  register(id: string) {
+    this.modals.push({ id, visible: false });
+  }
+
+  unregister(id: string) {
+    this.modals = this.modals.filter((modal) => modal.id !== id);
+  }
+}
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  @Input() modalID = '';
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
+  ngOnInit(): void {
+    // Append the modal to the root of the app. Last place in the body element
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
+  }
 }

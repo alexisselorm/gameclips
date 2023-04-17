498a1cd1019747c6463415031bb57ad88722f6ff
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-04-17 11:39:39 +0200

    Forms Form groups and form controls Connect UI forms to form controls

--- src/app/shared/tabs-container/tabs-container.component.spec.ts
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -1,23 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TabsContainerComponent } from './tabs-container.component';
+import { TabComponent } from '../tab/tab.component';
+import { QueryList } from '@angular/core';
 
 describe('TabsContainerComponent', () => {
   let component: TabsContainerComponent;
-  let fixture: ComponentFixture<TabsContainerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TabsContainerComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(TabsContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new TabsContainerComponent();
+    component.tabs = new QueryList<TabComponent>();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should select tab', () => {
+    const tab1 = new TabComponent();
+    const tab2 = new TabComponent();
+    component.tabs.reset([tab1, tab2]);
+    component.selectTab(tab1);
+    expect(tab1.active).toBe(true);
+    expect(tab2.active).toBe(false);
+  });
+
+  it('should select first tab if no active tabs', () => {
+    const tab1 = new TabComponent();
+    const tab2 = new TabComponent();
+    component.tabs.reset([tab1, tab2]);
+    component.ngAfterContentInit();
+    expect(tab1.active).toBe(true);
+    expect(tab2.active).toBe(false);
   });
 });

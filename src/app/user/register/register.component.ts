da0a94e40c7b407551531309814bf6a669297999
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-04-17 21:52:41 +0200

    Validating numbers Validating and masking phone numbers Disable button if form is invalid

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,10 +8,23 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent {
   registerForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    email: new FormControl('', [Validators.email]),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+      ),
+    ]),
+    confirm_password: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(13),
+    ]),
   });
 }

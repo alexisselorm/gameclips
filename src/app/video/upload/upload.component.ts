117b0a67e15fc4b65a445aa9ecda4711dd6610eb
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-05 17:45:39 +0200

    Handling files from events

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent {
+  isDragover = false;
+  file: File | null = null;
+  storeFile($event: Event) {
+    this.isDragover = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) as File;
 
+    if (!this.file || this.file.type != 'video/mp4') {
+    }
+
+    //Upload and store file
+    console.log(this.file);
+  }
 }

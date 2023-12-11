67abc3b1a1b0e814b4d3783326355e7474529ba5
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-11 10:52:43 +0200

    Toggling modals

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import IClip from 'src/app/models/clip.model';
 import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -15,7 +16,8 @@ export class ManageComponent implements OnInit {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modalService: ModalService
   ) {}
 
   ngOnInit(): void {
@@ -42,5 +44,11 @@ export class ManageComponent implements OnInit {
       relativeTo: this.activatedRoute,
       queryParams: { sort: value },
     });
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault();
+
+    this.modalService.toggleModal('editClip');
   }
 }

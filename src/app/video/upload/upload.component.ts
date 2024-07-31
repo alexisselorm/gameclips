fa397e3fbe5492d2f08926471165a10acb78909b
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-07-31 15:11:06 +0200

    FFmpeg service. Downgrade ffmpeg version

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -10,6 +10,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
 import { Router } from '@angular/router';
+import { FfmpegService } from 'src/app/services/ffmpeg.service';
 
 @Component({
   selector: 'app-upload',
@@ -21,11 +22,13 @@ export class UploadComponent implements OnDestroy {
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
     private clipsService: ClipService,
-    private router: Router
+    private router: Router,
+    protected ffmpegService: FfmpegService
   ) {
     auth.user.subscribe((user) => {
       this.user = user;
     });
+    this.ffmpegService.init();
   }
 
   title = new FormControl('', [Validators.required, Validators.minLength(3)]);

d79dbc820675fe633898dfec102cfb6c8a891a4d
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-08-02 13:02:18 +0000

    Videojs and initiazling video player

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import videojs from 'video.js';
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
@@ -8,8 +8,20 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ClipComponent implements OnInit {
   id = '';
+  @ViewChild('videoPlayer', {
+    static: true,
+  })
+  target?: ElementRef;
+  player?: videojs.Player;
   constructor(private activatedRoute: ActivatedRoute) {}
   ngOnInit(): void {
+    this.player = videojs(this.target?.nativeElement, {
+      controls: true,
+      autoplay: true,
+      sources: [
+        { src: `https://example.com/video/${this.id}.mp4`, type: 'video/mp4' },
+      ],
+    });
     this.activatedRoute.params.subscribe((params: Params) => {
       this.id = params['id'];
     });

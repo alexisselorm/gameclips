5623879ea9fb5cf0241cc6229a24384969a4cf9e
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-08-02 16:57:43 +0000

    Lazy loading video module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
@@ -30,7 +29,6 @@ import { FbTimeStampPipe } from './pipes/fb-time-stamp.pipe';
   ],
   imports: [
     BrowserModule,
-    VideoModule,
     AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),

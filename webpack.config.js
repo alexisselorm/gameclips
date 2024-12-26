3616d971a9382ea24f84381d0a11689da140daef
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-12-26 21:32:33 +0200

    .vercel env

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   plugins: [
     new Dotenv({
-      path: './.env', // Path to your .env file
+      path: './.vercel/.env', // Path to your .env file
       safe: true, // Load .env.example to verify required variables
     }),
   ],
